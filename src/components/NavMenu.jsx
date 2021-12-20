6526ba46ebf8988294f4e39f69c29b08a3a81a8d
Ben Arts <45427540+wicling@users.noreply.github.com>
2021-12-20 13:58:16 +0100

    first bottom nav bar

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,33 +1,42 @@
 import {NavLink} from "react-router-dom";
-import { useCallback } from "react";
-import { useLogout, useSession } from "../contexts/AuthProvider";
+import {useCallback} from "react";
+import {useLogout, useSession} from "../contexts/AuthProvider";
+import {IoCalendarOutline, IoExitOutline} from "react-icons/io5";
+import {useLocation} from 'react-router-dom';
 
 const NavItem = ({
     to,
-    label
+    label = "",
+    children
 }) => (
-    <span>
+    <div>
         <NavLink
             to={to}
-            className={`hover:text-blue-500 ${(navData) => navData.isActive ? "text-blue-500 cursor-default" : ''}`}
         >
+            {children}
             {label}
         </NavLink>
-    </span>
+    </div>
 );
 
 export default function NavMenu()
 {
-    const { isAuthed } = useSession();
+    const {isAuthed} = useSession();
     const logout = useLogout();
 
-    const handleLogout = useCallback(() => {
+    const path = process.env.PUBLIC_URL + '/logo outline white dik.svg';
+    const path2 = process.env.PUBLIC_URL + '/logo outline yellow dik.svg';
+
+    const {pathname} = useLocation();
+
+    const handleLogout = useCallback(() =>
+    {
         logout();
-      }, [logout]);
+    }, [logout]);
 
     return (
         <>
-            <nav className="flex space-x-6">
+            <nav className="flex space-x-6 nav-menu">
 
                 {
                     !isAuthed ? (
@@ -36,12 +45,22 @@ export default function NavMenu()
                         </>
                     ) : (
                         <>
-                            <NavItem to="/dagen" label="Dagen" />
-                            <NavItem to="/leden" label="Leden" />
-                            <button className={`hover:text-blue-500` } onClick={handleLogout}>
-                                Sign out
+                            <button onClick={handleLogout} className="nav-btn">
+                                <IoExitOutline size={40}
+                                    />
                             </button>
-                        </>
+                            <NavItem to="/dagen" >
+                                <div className={`${pathname === "/dagen" ? "nav-btn-act" : "nav-btn"}`} >
+                                    <IoCalendarOutline size={40} />
+                                </div>
+                            </NavItem>
+                            <NavItem to="/leden">
+                                <img alt="leden logo"
+                                    style={{width: 40}}
+                                    src={pathname === "/leden" ? path2 : path}
+                                    onMouseOver={e => (e.currentTarget.src = path2)}
+                                    onMouseOut={e => (e.currentTarget.src = pathname === "/leden" ? path2 : path)} />
+                            </NavItem></>
                     )
                 }
             </nav>
