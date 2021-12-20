0b4f80af130317d561bc14ccc08e44c3d6485a26
Ben Arts <45427540+wicling@users.noreply.github.com>
2021-12-20 19:52:52 +0100

    aanwezigheid toggle

--- src/pages/AanwezigheidForm.jsx
+++ src/pages/AanwezigheidForm.jsx
@@ -31,11 +31,16 @@ export default function AanwezigheidForm()
           {leden.map((lid) => <option key={lid.id} value={lid.id}>{lid.voornaam + ' ' + lid.achternaam}</option>)}
         </select>
 
-        <label htmlFor="aanwezig">Aanwezig(1)/Afwezig(0)</label>
-        <input id="aanwezig" type="aanwezig" {...register("aanwezig")} />
+
+        <label htmlFor="aanwezig">
+        Aanwezig:
+        <input is="gui-switch" type="checkbox" role="switch" id="aanwezig" {...register("aanwezig")}/>
+      </label>
+
         <input type="submit" value="create" />
         {error && <pre className="text-red-600">{error.message}</pre>}
       </form>
+
     </>
   );
 };
